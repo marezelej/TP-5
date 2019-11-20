75b0315c694e7fae52bffb92b6ef385ffe6ef407
agustin otero <oteroagustin95@gmail.com>
2019-11-20 18:56:29 -0300

    Se "Agrego"(copio) implementacion de la clase Bank sugerida por el profesor.

--- Ejercicio 2/AccountManager/Bank.cs
+++ Ejercicio 2/AccountManager/Bank.cs
@@ -1,21 +1,87 @@
-﻿using System;
+﻿using AutoMapper;
+using System.Collections.Generic;
+using AccountManager.DAL;
+using AccountManager.Domain;
+using AccountManager.IO;
+using AccountManager.DAL.EntityFramework;
+using System;
+
 
 namespace AccountManager
 {
-    public class Bank
+      public class Bank
     {
-        public Bank() { }
-           
+
+        private static readonly IMapper cMapper;
+
+        static Bank()
+        {
+            var mConfiguration = new MapperConfiguration(pConfiguration =>
+            {
+                pConfiguration.CreateMap<AccountMovement, AccountMovementDTO>();
+            });
+
+            cMapper = mConfiguration.CreateMapper();
+        }
+
         public IEnumerable<AccountDTO> GetClientAccounts(int pClientId)
         {
-            List<AccountMovmentDTO> mAccountMovements = new List<AccountMovmentDTO>();
+            IList<AccountDTO> mAccounts;
 
+            using (var bDbContext = new AccountManagerDbContext())
+            {
+                using (IUnitOfWork bUoW = new UnitOfWork(bDbContext))
+                {
+                    var bClient = bUoW.ClientRepository.Get(pClientId);
+
+                    if (bClient != null && bClient.Accounts.Count > 0)
+                    {
+                        mAccounts = new List<AccountDTO>(bClient.Accounts.Count);
+
+                        foreach (var bAccount in bClient.Accounts)
+                        {
+                            mAccounts.Add(new AccountDTO
+                            {
+                                Id = bAccount.Id,
+                                Name = bAccount.Name,
+                                Balance = bAccount.GetBalance(),
+                                OverDraftLimit = bAccount.OverdraftLimit
+                            });
+                        }
+                    }
+                    else
+                    {
+                        mAccounts = new List<AccountDTO>();
+                    }
+                }
+            }
+
+            return mAccounts;
         }
-        public IEnumerable<AccountMovementDTO> GetAccountMovements(int pAcountID)
+
+        public IEnumerable<AccountMovementDTO> GetAccountMovements(int pAccountId)
         {
+            IList<AccountMovementDTO> mAccountMovements;
 
+            using (var bDbContext = new AccountManagerDbContext())
+            {
+                using (IUnitOfWork bUoW = new UnitOfWork(bDbContext))
+                {
+                    var bAccount = bUoW.AccountRepository.Get(pAccountId);
+
+                    if (bAccount != null && bAccount.Movements.Count > 0)
+                    {
+                        mAccountMovements = cMapper.Map<IList<AccountMovementDTO>>(bAccount.Movements);
+                    }
+                    else
+                    {
+                        mAccountMovements = new List<AccountMovementDTO>();
+                    }
+                }
+            }
+
+            return mAccountMovements;
         }
-
 
     }
 }
